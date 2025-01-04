786ec6d75222871c29479108b7dadd4fd21f0dd6
Ather Saeed <athers896@gmail.com>
2025-01-04 18:51:37 -0500

    added new files (with errors), work in progress

---
+++ api.py
@@ -0,0 +1,57 @@
+# imports
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api, reqparse, fields, marshal_with, abort
+
+# flask app
+app = Flask(__name__)
+# setting up db using sqlalchemy 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+api = Api(app)
+
+class UserModel(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), unique = True, nullable = False)
+    email = db.Column(db.String(80), unique = True, nullable = False)
+
+    def __repr__(self):
+        return f"User(name = {self.name}, email = {self.name})"
+    
+
+user_args = reqparse.RequestParser()
+user_args.add_argument('name', type=str, required = True, help="Name cannot be blank")
+user_args.add_argument('email', type=str, required = True, help="Email cannot be blank")
+
+userFields = {
+    'id':fields.Integer,
+    'name':fields.String,
+    'email':fields.String
+}
+
+
+class Users(Resource):
+    @marshal_with(userFields)
+    def get(self):
+        users = UserModel.query.all()
+        return users
+    
+    @marshal_with
+    def post(self):
+        args = user_args
+        user = UserModel(name=args["name"], email=args["email"])
+        db.session.add(user)
+        db.session.commit()
+        users = UserModel.query.all()
+        return users, 201
+
+api.add_resource(Users, '/api/users/')
+
+# setting up home route
+@app.route('/')
+def home():
+    return '<h1>Flask Rest API</h1>'
+
+# running the app with debug (only for dev, never for prod)
+if __name__ == '__main__':
+    app.run(debug=True)
